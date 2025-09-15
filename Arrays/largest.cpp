58b987e365255650d90be35fcdb2f678136b2276
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2025-09-15 23:46:29 +0500

    Largest of an array

--- Arrays/largest.cpp
+++ Arrays/largest.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int main() {
 
     int size = 5;
-    int array[size] = {54, 65, 4, 24, 66};
+    int array[size] = {54, 65, 4, 24, 66, 66};
 
-    
+    int largest = INT_MIN;
+    for(int i=0; i<size; i++) {
+        if(array[i]>largest) {
+            largest = array[i];
+        }
+    }    
 
-
-
-
-
-
+    cout << "Largest: " << largest << endl;
 
     return 0;
 }
