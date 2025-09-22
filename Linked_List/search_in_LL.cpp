c17b885da4196195149ccfa8dd8e9d7890a4782a
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2025-09-22 19:45:01 +0500

    Search in LL

---
+++ Linked_List/search_in_LL.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    void push_front(int value) {
+        Node* newNode = new Node(value);
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    int search(int key) {
+        Node* temp = head;
+        int index = 0;
+        while(temp != NULL) {
+            if(temp->data == key) {
+                return index;
+            }
+            temp = temp->next;
+            index++;
+        }
+        return -1;
+    }
+
+    void printLL() {
+        Node* temp = head;
+        while(temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+
+    List ll;
+    ll.push_front(300);
+    ll.push_front(500);
+    ll.push_front(700);
+
+    ll.printLL();
+
+    int index = ll.search(800);
+    if(index >= 0) {
+        cout << "Found at index: " << index << endl;
+    } else {
+        cout << "Not Found!" << endl;
+    }
+    
+    return 0;
+}
